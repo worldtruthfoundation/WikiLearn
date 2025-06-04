b22ffffa9f2bb701fbae16f8ef9ce04623025521
worldtruthfoundation <worldtruthfoundation@gmail.com>
2025-06-04 18:48:06 +0200

    New

--- app.py
+++ app.py
@@ -10,9 +10,5 @@ logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 app.secret_key = os.environ.get("SESSION_SECRET", "wikilearn_dev_secret_key")
 
-# 👇  всё маршрутизацией занимается routes.py
-from routes import *           # noqa: E402,F401
 
-if __name__ == "__main__":
-    # при локальной разработке → Ctrl-C останавливает
-    app.run(host="0.0.0.0", port=5000, debug=True)
+from routes import *
